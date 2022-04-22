0b1c0297e8c075e1cc68c23d2061437b6a21babd
Conradgabe <98839344+Conradgabe@users.noreply.github.com>
2022-04-21 23:06:27 -1100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const server = http.createServer((req, res) => {
 
     res.setHeader('Content-Type', 'text/html');
 
+    const osinfo = JSON.stringify({
+        hostname: os.hostname(),
+        platform: os.platform(),
+        architecture: os.arch(),
+        numberofCPUS: os.cpus(),
+        networkInterfaces: os.networkInterfaces(),
+        uptime: os.uptime(),
+    });
+
     let path = './pages'
     switch(req.url){
         case '/':
@@ -20,34 +29,23 @@ const server = http.createServer((req, res) => {
             path += '/about.html';
             res.statusCode = 200;
             break;
+
+        case '/sys':
+                res.statusCode = 201;
+                res.setHeader('Content-Type', 'text/plain');
+        
+                fs.writeFile('osinfo.json', osinfo, (err) => {
+                    if(err){
+                        console.log(error);
+                    };
+                });
+                res.write('Your os have been saved successfully');
+                res.end();
+            
         default:
             path += '/404.html';
             res.statusCode = 404;
             break;
-    };
-
-    const urlPath = req.url;
-    
-    const osinfo = JSON.stringify({
-        hostname: os.hostname(),
-        platform: os.platform(),
-        architecture: os.arch(),
-        numberofCPUS: os.cpus(),
-        networkInterfaces: os.networkInterfaces(),
-        uptime: os.uptime(),
-    });
-
-    if (urlPath === '/sys'){
-        res.statusCode = 201;
-        res.setHeader('Content-Type', 'text/plain');
-
-        fs.writeFile('osinfo.json', osinfo, (err) => {
-            if(err){
-                console.log(error);
-            };
-        });
-        res.write('Your os have been saved successfully');
-        res.end();
     };
 
     fs.readFile(path, (err, data) => {
